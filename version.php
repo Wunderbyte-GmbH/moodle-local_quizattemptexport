afb334a90c507e5252ac723ea41f0d342fecf580
mm-poustini <mmp1368@gmail.com>
2026-08-31 09:26:32 +0200

    Version (2026083100)

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2026081900;
+$plugin->version = 2026083100;
 $plugin->release = 'v2.0-r1';
 $plugin->requires  = 2022112800;
 $plugin->component = 'local_quizattemptexport';
